92b3d956384227bbbecd6a31547e935c87767a39
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-03-09 23:10:43 +0100

    added some dummy house listings data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,5 +9,41 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  houseListings = [
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Lagos',
+      starRating: '4.5',
+      favorite: true,
+      price: "450"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Abuja',
+      starRating: '4.0',
+      price: "4500"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'POgun',
+      starRating: '3.5',
+      favorite: false,
+      price: "1750"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Ibadan',
+      starRating: '3.5',
+      favorite: false,
+      price: "450"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Osogbo',
+      starRating: '3.5',
+      favorite: true,
+      price: "750"
+    },
+  ]
 
 }
