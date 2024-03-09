8fc4e1b76f686b745d442e58795237140667ecbf
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-03-09 23:54:47 +0100

    figured out how to import imgages in a ts file; by specifying the image path

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -34,8 +34,8 @@ export class HomepageComponent {
       price: "4500"
     },
     {
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
-      location: 'POgun',
+      src: '../../assets/images/airbnb.png',
+      location: 'Ogun',
       starRating: '3.5',
       favorite: false,
       price: "1750"
@@ -53,6 +53,41 @@ export class HomepageComponent {
       starRating: '3.5',
       favorite: true,
       price: "750"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Kaduna',
+      starRating: '3.5',
+      favorite: false,
+      price: "1750"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Zamfara',
+      starRating: '3.5',
+      favorite: false,
+      price: "1750"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Abakaliki',
+      starRating: '3.5',
+      favorite: false,
+      price: "1750"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Imo',
+      starRating: '3.5',
+      favorite: false,
+      price: "1750"
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSv6CyqS-g3kJDKjhit7sQ-Us0somkda0X5w&usqp=CAU',
+      location: 'Abia',
+      starRating: '3.5',
+      favorite: false,
+      price: "1750"
     },
   ]
 
