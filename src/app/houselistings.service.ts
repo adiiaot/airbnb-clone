963b0876d039a1eaa40303c2c5bbf78192ffeb58
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-03-17 18:43:56 +0100

    added appropriate images to the views filter

--- src/app/houselistings.service.ts
+++ src/app/houselistings.service.ts
@@ -9,7 +9,7 @@ export class HouselistingsService {
   getHouses(): HouseListings[] {
     return [
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house6.jfif','../assets/images/house7.jfif','../assets/images/house8.jfif' ] ,
+photos: ['../assets/images/az1.jpg', '../assets/images/az2.jfif','../assets/images/az3.jfif','../assets/images/az4.jfif','../assets/images/az5.jfif',] ,
         location: 'Lagos',
         starRating: '4.82',
         favorite: true,
@@ -18,7 +18,7 @@ export class HouselistingsService {
         views: 'amazing views'
       },
       {
-        photos: ['../assets/images/house8.jfif', '../assets/images/house7.jfif','../assets/images/house6.jfif','../assets/images/house5.jfif','../assets/images/house4.jfif','../assets/images/house3.jfif','../assets/images/house2.jfif','../assets/images/house1.jfif' ] ,
+        photos: ['../assets/images/surf1.jfif', '../assets/images/surf2.jfif','../assets/images/surf3.jpg','../assets/images/surf4.jfif','../assets/images/surf5.jfif',] ,
         location: 'Ogun',
         starRating: '5.0',
         favorite: true,
@@ -27,7 +27,7 @@ export class HouselistingsService {
         views: 'surfing'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house5.jfif','../assets/images/house2.jfif','../assets/images/house6.jfif','../assets/images/house3.jfif','../assets/images/house7.jfif','../assets/images/house4.jfif','../assets/images/house8.jfif' ] ,
+        photos: ['../assets/images/trend1.jfif', '../assets/images/trend2.jfif','../assets/images/trend3.jfif','../assets/images/trend4.jfif','../assets/images/trend5.jfif',] ,
         location: 'Awka',
         starRating: '4.88',
         favorite: false,
@@ -36,7 +36,7 @@ export class HouselistingsService {
         views: 'trending'
       },
       {
-        photos: ['../assets/images/house2.jfif', '../assets/images/house4.jfif','../assets/images/house6.jfif','../assets/images/house8.jfif','../assets/images/house1.jfif','../assets/images/house3.jfif','../assets/images/house5.jfif','../assets/images/house7.jfif' ] ,
+        photos: ['../assets/images/new1.jfif', '../assets/images/new2.jfif','../assets/images/new3.jfif','../assets/images/new4.jfif','../assets/images/new5.jfif',] ,
         location: 'Lafenwa',
         starRating: '4.94',
         favorite: false,
@@ -45,7 +45,7 @@ export class HouselistingsService {
         views: 'new'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/earth1.jfif', '../assets/images/earth2.jfif','../assets/images/earth3.jfif','../assets/images/earth4.jfif','../assets/images/earth5.jfif',] ,
         location: 'Jigawa',
         starRating: '4.5',
         favorite: true,
@@ -55,7 +55,7 @@ export class HouselistingsService {
       },
 
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house1.jfif','../assets/images/house1.jfif','../assets/images/house1.jfif','../assets/images/house1.jfif','../assets/images/house1.jfif','../assets/images/house1.jfif','../assets/images/house1.jfif' ] ,
+        photos: ['../assets/images/farm1.jfif', '../assets/images/farm2.jfif','../assets/images/farm3.jfif','../assets/images/farm4.jfif','../assets/images/farm5.jfif',] ,
         location: 'Taraba',
         starRating: '4.0',
         favorite: true,
@@ -64,7 +64,7 @@ export class HouselistingsService {
         views: 'farms'
       },
       {
-        photos: ['../assets/images/house8.jfif', '../assets/images/house7.jfif','../assets/images/house6.jfif','../assets/images/house5.jfif','../assets/images/house4.jfif','../assets/images/house3.jfif','../assets/images/house2.jfif','../assets/images/house1.jfif' ] ,
+        photos: ['../assets/images/island1.jfif', '../assets/images/island2.jfif','../assets/images/island3.jfif','../assets/images/island4.jfif','../assets/images/island5.jfif',] ,
         location: 'Osun',
         starRating: '4.93',
         favorite: false,
@@ -73,7 +73,7 @@ export class HouselistingsService {
         views: 'islands'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house5.jfif','../assets/images/house2.jfif','../assets/images/house6.jfif','../assets/images/house3.jfif','../assets/images/house7.jfif','../assets/images/house4.jfif','../assets/images/house8.jfif' ] ,
+        photos: ['../assets/images/beach1.jfif', '../assets/images/beach2.jfif','../assets/images/beach3.jfif','../assets/images/beach4.jfif','../assets/images/beach5.jfif',] ,
         location: 'Ibadan',
         starRating: '2.87',
         favorite: true,
@@ -82,7 +82,7 @@ export class HouselistingsService {
         views: 'beachfront'
       },
       {
-        photos: ['../assets/images/house2.jfif', '../assets/images/house4.jfif','../assets/images/house6.jfif','../assets/images/house8.jfif','../assets/images/house1.jfif','../assets/images/house3.jfif','../assets/images/house5.jfif','../assets/images/house7.jfif' ] ,
+        photos: ['../assets/images/design1.jfif', '../assets/images/design2.jfif','../assets/images/design3.jfif','../assets/images/design4.jfif','../assets/images/design5.jfif',] ,
         location: 'Yobe',
         starRating: '5.0',
         favorite: true,
@@ -91,7 +91,7 @@ export class HouselistingsService {
         views: 'design'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/cabin1.jfif', '../assets/images/cabin2.jfif','../assets/images/cabin3.jfif','../assets/images/cabin4.jfif','../assets/images/cabin5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
@@ -103,7 +103,7 @@ export class HouselistingsService {
 
 
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/camp1.jfif', '../assets/images/camp2.jfif','../assets/images/camp3.jfif','../assets/images/camp4.jfif','../assets/images/camp5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
@@ -112,7 +112,7 @@ export class HouselistingsService {
         views: 'campers'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/pool1.jfif', '../assets/images/pool2.jfif','../assets/images/pool3.jfif','../assets/images/pool4.jfif','../assets/images/pool5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
@@ -121,16 +121,16 @@ export class HouselistingsService {
         views: 'amazing pools'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/alien1.jfif', '../assets/images/alien2.jfif','../assets/images/alien3.jfif','../assets/images/alien4.jfif','../assets/images/alien5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
         priceAfterTax: '1,250',
         priceBeforeTax: '1,100',
-        views: 'omg'
+        views: 'omg!'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/arctic1.jfif', '../assets/images/arctic2.jfif','../assets/images/arctic3.jfif','../assets/images/arctic4.jfif','../assets/images/arctic5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
@@ -139,7 +139,7 @@ export class HouselistingsService {
         views: 'arctic'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/cave1.jfif', '../assets/images/cave2.jfif','../assets/images/cave3.jfif','../assets/images/cave4.jfif','../assets/images/cave5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
@@ -148,7 +148,7 @@ export class HouselistingsService {
         views: 'caves'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/tropic1.jfif', '../assets/images/tropic2.jfif','../assets/images/tropic3.jfif','../assets/images/tropic4.jfif','../assets/images/tropic5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
@@ -157,7 +157,7 @@ export class HouselistingsService {
         views: 'tropical'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/room1.jfif', '../assets/images/room2.jfif','../assets/images/room3.jfif','../assets/images/room4.jfif','../assets/images/room5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
@@ -166,7 +166,7 @@ export class HouselistingsService {
         views: 'rooms'
       },
       {
-        photos: ['../assets/images/house1.jfif', '../assets/images/house2.jfif','../assets/images/house3.jfif','../assets/images/house4.jfif','../assets/images/house5.jfif','../assets/images/house8.jfif','../assets/images/house7.jfif','../assets/images/house6.jfif' ] ,
+        photos: ['../assets/images/lake1.jfif', '../assets/images/lake2.jfif','../assets/images/lake3.jfif','../assets/images/lake4.jfif','../assets/images/lake5.jfif',] ,
         location: 'Damaturu',
         starRating: '4.97',
         favorite: false,
